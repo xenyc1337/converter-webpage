5fbd8b1b709b11cfe3aec45211e9c0600ad9ca6b
xenyc1337 <aleqsandredoxnadze@gmail.com>
2023-05-02 22:07:36 +0400

    Number Converter Beta

---
+++ script.js
@@ -0,0 +1,56 @@
+function openTab(evt, tabName) {
+   var i, tabContent, tabs;
+   tabContent = document.getElementsByClassName("tab-content");
+
+   for (i = 0; i < tabContent.length; i++) {
+       tabContent[i].style.display = "none";
+   }
+
+   tabs = document.getElementsByClassName("tab");
+   for (i = 0; i < tabs.length; i++) {
+       tabs[i].style.backgroundColor = "#3498db";
+   }
+
+   evt.currentTarget.style.backgroundColor = "#1e7ab7";
+   document.getElementById(tabName).style.display = "block";
+}
+
+function convert(base) {
+   var input = document.getElementById(base.toLowerCase()).value;
+   var result = { binary: "0", decimal: "0", octal: "0", hexadecimal: "0" };
+
+   switch(base) {
+       case "Binary":
+           result.decimal = parseInt(input, 2).toString(10);
+           result.octal = parseInt(input, 2).toString(8);
+           result.hexadecimal = parseInt(input, 2).toString(16);
+           break;
+       case "Decimal":
+           result.binary = parseInt(input, 10).toString(2);
+           result.octal = parseInt(input, 10).toString(8);
+           result.hexadecimal = parseInt(input, 10).toString(16);
+           break;
+       case "Octal":
+           result.binary = parseInt(input, 8).toString(2);
+           result.decimal = parseInt(input, 8).toString(10);
+           result.hexadecimal = parseInt(input, 8).toString(16);
+           break;
+       case "Hexadecimal":
+           result.binary = parseInt(input, 16).toString(2);
+           result.decimal = parseInt(input, 16).toString(10);
+           result.octal = parseInt(input, 16).toString(8);
+           break;
+   }
+
+   document.getElementById(base).querySelector(".result").innerHTML = `
+       Binary: ${result.binary} <br>
+       Decimal: ${result.decimal} <br>
+       Octal: ${result.octal} <br>
+       Hexadecimal: ${result.hexadecimal}
+   `;
+}
+
+// Initialize the first tab by default
+document.addEventListener("DOMContentLoaded", function() {
+   openTab({currentTarget: document.getElementsByClassName("tab")[0]}, "Binary");
+});
